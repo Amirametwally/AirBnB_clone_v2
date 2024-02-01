4444a48fa0b6f58143acc83522b0e65f32672984
Amirametwally <amira.metwally56@gmail.com>
2024-02-01 23:40:23 +0200

    Compress before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,7 +6,7 @@ Clone repo, using the function do_pack
 """
 
 from datetime import datetime
-from fabric.connection import Connection
+from fabric.api import local
 from os.path import isdir
 
 
@@ -15,9 +15,9 @@ def do_pack():
     try:
         date = datetime.now().strftime("%Y%m%d%H%M%S")
         if isdir("versions") is False:
-            Connection("mkdir versions")
+            local("mkdir versions")
         file_name = "versions/web_static_{}.tgz".format(date)
-        Connection("tar -cvzf {} web_static".format(file_name))
+        local("tar -cvzf {} web_static".format(file_name))
         return file_name
     except:
         return None
