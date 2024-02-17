26f13fb76cf3a07e07d067e851bb06bc17ea5b93
Amirametwally <amira.metwally56@gmail.com>
2024-02-18 01:21:59 +0200

    deploy_web_static

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-"""
-Fabric script based on the file 2-do_deploy_web_static.py that creates and
-distributes an archive to the web servers
+"""script based on the file 1-pack_web_static.py that distributes an
+archive to the web servers
 """
 
-from fabric.api import env, local, put, run
-from datetime import datetime
+from fabric.api import put, run, env, local
 from os.path import exists, isdir
-env.hosts = ['54.237.99.66', '34.202.159.136']
+from datetime import datetime
+
+
+env.hosts = ["54.237.99.66", "34.202.159.136"]
 
 
 def do_pack():
@@ -24,28 +25,29 @@ def do_pack():
 
 
 def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
+    """distributes an archive to servers"""
+    if not exists(archive_path):
+        print("Archive does not exist.")
         return False
     try:
         file_n = archive_path.split("/")[-1]
         no_ext = file_n.split(".")[0]
         path = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        run('sudo mkdir -p {}{}/'.format(path, no_ext))
-        run('sudo tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('sudo rm /tmp/{}'.format(file_n))
-        run('sudo mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('sudo rm -rf {}{}/web_static'.format(path, no_ext))
-        run('sudo rm -rf /data/web_static/current')
-        run('sudo ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        put(archive_path, "/tmp/")
+        run("sudo mkdir -p {}{}/".format(path, no_ext))
+        run("sudo tar -xzf /tmp/{} -C {}{}/".format(file_n, path, no_ext))
+        run("sudo rm /tmp/{}".format(file_n))
+        run("sudo mv {0}{1}/web_static/* {0}{1}/".format(path, no_ext))
+        run("sudo rm -rf {}{}/web_static".format(path, no_ext))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s {}{}/ /data/web_static/current".format(path, no_ext))
         return True
-    except:
+    except Exception as e:
         return False
 
 
 def deploy():
-    """creates and distributes an archive to the web servers"""
+    """creates and distributes archive to servers"""
     archive_path = do_pack()
     if archive_path is None:
         return False
