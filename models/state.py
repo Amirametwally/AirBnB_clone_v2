61bea6b27f525a498d65cdb7e21c64e5230e68f1
Amirametwally <amira.metwally56@gmail.com>
2024-02-17 21:55:48 +0200

    file state

--- models/state.py
+++ models/state.py
@@ -1,10 +1,36 @@
 #!/usr/bin/python3
-"""class state that inherit from Basemodel"""
-
-from .base_model import BaseModel
+"""Class state that inherit from Basemodel"""
 
 
-class State(BaseModel):
-    """public class attribute"""
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+import models
+from models.base_model import BaseModel, Base
+from models.city import City
+from os import getenv
 
-    name = ""
+
+class State(BaseModel, Base):
+    """Representation of state"""
+
+    if models.storage_t == "db":
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state")
+    else:
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    if models.storage_t != "db":
+
+        @property
+        def cities(self):
+            city_list = []
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
