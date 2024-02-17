9d12d1c1c3618dba647fc6b2847074f3604cfcf2
Amirametwally <amira.metwally56@gmail.com>
2024-02-17 23:46:21 +0200

    done

--- models/state.py
+++ models/state.py
@@ -1,26 +1,30 @@
 #!/usr/bin/python3
 """Class state that inherit from Basemodel"""
 
-
+import models
 from os import getenv
 from models.base_model import BaseModel, Base
 
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.city import City
-from models.engine.file_storage import storage
+
 
 class State(BaseModel, Base):
     """class State inherits from BaseModel and Base"""
 
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="all, delete-orphan")
 
-    @property
-    def cities(self):
-        """Getter for cities linked to the current State"""
-        if getenv('HBNB_TYPE_STORAGE') == 'db':
-            return storage.all(City).values()
-        else:
-            return [city for city in storage.all(City).values() if city.state_id == self.id]
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship("City", backref="state", cascade="all, delete")
+    else:
+
+        @property
+        def cities(self):
+            """Get a list of City instances"""
+            city_list = []
+            for city in models.storage.all(City).values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
