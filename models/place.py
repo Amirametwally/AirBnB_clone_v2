fd7ca6b8ba99dc6315c8c78c0201c38c97ae0037
Amirametwally <amira.metwally56@gmail.com>
2024-02-18 00:53:52 +0200

    place class

--- models/place.py
+++ models/place.py
@@ -1,19 +1,35 @@
 #!/usr/bin/python3
 """class place that inherit from BaseModel"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey, Integer, Float, Table
+from sqlalchemy.orm import relationship
+from models.review import Review
+from models.amenity import Amenity
+import models
+from os import getenv
+
+type_of_storage = getenv("HBNB_TYPE_STORAGE")
+
+if type_of_storage == "db":
+    place_amenity = Table(
+        "place_amenity",
+        Base.metadata,
+        Column(
+            "place_id",
+            String(60),
+            ForeignKey("places.id"),
+            primary_key=True,
+            nullable=False,
+        ),
+        Column(
+            "amenity_id",
+            String(60),
+            ForeignKey("amenities.id"),
+            primary_key=True,
+            nullable=False,
+        ),
+    )
 
 
 class Place(BaseModel):
     """public class attributa"""
-
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
-    amenity_ids = []
