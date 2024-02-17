ebafbb8bfe2c4500b8fb921bede87bd0f19dc1cf
Amirametwally <amira.metwally56@gmail.com>
2024-02-18 00:57:03 +0200

    review claa

--- models/review.py
+++ models/review.py
@@ -1,12 +1,31 @@
 #!/usr/bin/python3
 """class Review that inherit from BaseModel"""
 
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from os import getenv
+
+
+type_of_storage = getenv("HBNB_TYPE_STORAGE")
 
 
 class Review(BaseModel):
     """public class attribute"""
 
-    place_id = ""
-    user_id = ""
-    text = ""
+    __tablename__ = "reviews"
+    if type_of_storage == "db":
+        text = Column(String(1024), nullable=False)
+        place_id = Column(
+            String(60),
+            ForeignKey("places.id"),
+            nullable=False,
+        )
+        user_id = Column(
+            String(60),
+            ForeignKey("users.id"),
+            nullable=False,
+        )
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
