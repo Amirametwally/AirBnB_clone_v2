5a277293eed587acc4cdac341adda27d4ad0ecd0
Amirametwally <amira.metwally56@gmail.com>
2024-02-18 18:34:12 +0200

    update

--- models/place.py
+++ models/place.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""class place that inherit from BaseModel"""
+"""
+    Define the class Place.
+"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey, Integer, Float, Table
 from sqlalchemy.orm import relationship
@@ -31,5 +33,98 @@ if type_of_storage == "db":
     )
 
 
-class Place(BaseModel):
-    """public class attributa"""
+class Place(BaseModel, Base):
+    """
+    Define the class Place that inherits from BaseModel.
+    """
+
+    __tablename__ = "places"
+    if type_of_storage == "db":
+        city_id = Column(
+            String(60),
+            ForeignKey("cities.id"),
+            nullable=False,
+        )
+        user_id = Column(
+            String(60),
+            ForeignKey("users.id"),
+            nullable=False,
+        )
+        name = Column(
+            String(128),
+            nullable=False,
+        )
+        description = Column(
+            String(1024),
+            nullable=True,
+        )
+        number_rooms = Column(
+            Integer,
+            nullable=False,
+            default=0,
+        )
+        number_bathrooms = Column(
+            Integer,
+            nullable=False,
+            default=0,
+        )
+        max_guest = Column(
+            Integer,
+            nullable=False,
+            default=0,
+        )
+        price_by_night = Column(
+            Integer,
+            nullable=False,
+            default=0,
+        )
+        latitude = Column(
+            Float,
+            nullable=True,
+        )
+        longitude = Column(
+            Float,
+            nullable=True,
+        )
+        reviews = relationship(
+            "Review",
+            backref="place",
+            cascade="all, delete-orphan",
+        )
+        amenities = relationship(
+            "Amenity",
+            secondary="place_amenity",
+            viewonly=False,
+            back_populates="place_amenities",
+            cascade="all",
+        )
+    else:
+        city_id = ""
+        user_id = ""
+        name = ""
+        description = ""
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.0
+        longitude = 0.0
+
+        @property
+        def reviews(self):
+            """Returns the list of Review"""
+            reviews = models.storage.all(Review)
+            reviews_lista = []
+            for review in reviews.values():
+                if review.place_id == self.id:
+                    reviews_lista.append(review)
+            return reviews_lista
+
+        @property
+        def amenities(self):
+            """Returns the list of Amenity"""
+            amenities = models.storage.all(Amenity)
+            amenities_lista = []
+            for amenity in amenities.values():
+                if amenity.id in self.amenity_ids:
+                    amenities_lista.append(amenity)
