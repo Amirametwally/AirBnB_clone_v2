761f6e37af459278b73d996b85903ff109be9ee9
Amirametwally <amira.metwally56@gmail.com>
2024-02-18 04:51:53 +0200

    initialization

--- models/__init__.py
+++ models/__init__.py
@@ -1,13 +1,35 @@
 #!/usr/bin/python3
-"""Instantiates a storage object.
-"""
+"""module: __init__"""
+
 from os import getenv
 
+from models.state import State
+from models.city import City
+from models.user import User
+from models.review import Review
+from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.place import Place
 
-if getenv("HBNB_TYPE_STORAGE") == "db":
+type_of_storage = getenv("HBNB_TYPE_STORAGE")
+
+if type_of_storage == "db":
     from models.engine.db_storage import DBStorage
+
     storage = DBStorage()
+    storage.reload()
 else:
     from models.engine.file_storage import FileStorage
+
     storage = FileStorage()
-storage.reload()
+    storage.reload()
+
+classes = {
+    "State": State,
+    "City": City,
+    "User": User,
+    "Review": Review,
+    "BaseModel": BaseModel,
+    "Amenity": Amenity,
+    "Place": Place,
+}
